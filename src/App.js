de0150d7eb8b4f3430f73350a5be76425ac2a395
ken-lyk <111368233+ken-lyk@users.noreply.github.com>
2022-08-28 20:44:16 +0800

    update app.js text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1>
+        Testing on the CI/CD for React project
+      </h1>
+      
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +21,7 @@ function App() {
         >
           Testing on the CI/CD for React project
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
