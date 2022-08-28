23806c9925dbc0fb7883563b60e5138aa36f94fb
ken-lyk <111368233+ken-lyk@users.noreply.github.com>
2022-08-28 19:31:28 +0800

    update title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing on the CI/CD for React project
         </a>
       </header>
     </div>
