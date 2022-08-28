91a03abd842d1e0f01221e88eecebd5fc522a1be
ken-lyk <111368233+ken-lyk@users.noreply.github.com>
2022-08-28 22:25:25 +0800

    remark

--- src/App.js
+++ src/App.js
@@ -10,17 +10,7 @@ function App() {
       
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Testing on the CI/CD for React project
-        </a>
+        WIP - Ken
       </header>
     </div>
   );
