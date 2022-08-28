0aaecd52da61cb1efde9b1b0976877d2f6a2d444
ken-lyk <111368233+ken-lyk@users.noreply.github.com>
2022-08-28 22:28:18 +0800

    update index file

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ function App() {
     <div className="App">
       <h1>
         Testing on the CI/CD for React project
+        <br/>
+        WIP - Ken
       </h1>
       
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        WIP - Ken
       </header>
     </div>
   );
