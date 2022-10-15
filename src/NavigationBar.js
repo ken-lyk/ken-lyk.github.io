b17dbef8101aa8bf2e1300c32ea4db158997cdbd
ken-lyk <111368233+ken-lyk@users.noreply.github.com>
2022-10-15 22:58:37 +0800

    fix The href attribute requires a valid value to be accessible.

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -46,7 +46,7 @@ export default class NavigationBar extends Component {
 
     renderListItem(title) {
         return <li>
-            <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{title}</a>
+            <a href="/" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{title}</a>
         </li>
     }
 }
