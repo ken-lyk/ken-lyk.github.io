cb8fa6977a310aab6c6546591c7240f0fe35280f
ken-lyk <111368233+ken-lyk@users.noreply.github.com>
2022-10-15 22:03:50 +0800

    restructure app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import './App.css';
-import React from 'react'
+import './App.css'; 
+import React, { Component } from 'react'
 import Home from './Home';
 import About from './About';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>
-        Testing on the CI/CD for React project
-        <br/>
-        WIP - Ken 
-      </h1>
-
-      <h1 className="text-5xl font-bold text-slate-500">
-      Hello world!
-    </h1>
-      
-      <Home/>
-      <About/>
-    </div>
-  );
+export default class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <h1>
+            Testing on the CI/CD for React project
+            <br/>
+            WIP - Ken 
+          </h1>
+    
+          <h1 className="text-5xl font-bold text-slate-500">
+          Hello world!
+        </h1>
+          
+          <Home/>
+          <About/>
+        </div>
+      );
+    }
 }
-
-export default App;
