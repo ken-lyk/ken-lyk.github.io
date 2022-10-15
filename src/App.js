3a0d906045a10225271666492468103960e7e7a7
ken-lyk <111368233+ken-lyk@users.noreply.github.com>
2022-10-15 22:56:50 +0800

    ui update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import './App.css'; 
 import React, { Component } from 'react'
-import Home from './Home';
-import About from './About';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import NavigationBar from './NavigationBar';
 
 export default class App extends Component {
     render() {
       return (
         <div className="App">
-          <h1>
-            Testing on the CI/CD for React project
-            <br/>
-            WIP - Ken 
-          </h1>
-    
+          <NavigationBar />
           <h1 className="text-5xl font-bold text-slate-500">
           Hello world!
         </h1>
